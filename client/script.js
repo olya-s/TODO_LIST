8ade651b4f5afde9da162d62d02ac948e9817c44
olya-s <osafronyuk85@gmail.com>
2020-11-15 20:34:21 +0200

    test task todo-list

---
+++ client/script.js
@@ -0,0 +1,408 @@
+async function onPageLoaded() {
+  let todosList = [];
+  let tasksList = [];
+  let token = localStorage.token;
+  let userId = localStorage.id;
+
+  const newTodo = document.querySelector('.add-todo-btn');
+  const container = document.querySelector('.container');
+
+  if (!token) {
+    createAuthForm(container);
+  }
+  else {
+    createLogoutButton(container);
+
+    getTodos(userId);
+    newTodo.style.visibility = 'visible';
+  }
+
+  function createAuthForm(container) {
+    const authForm = document.createElement('div');
+    authForm.classList.add('auth-form');
+    const inputsForm = document.createElement('div');
+    inputsForm.classList.add('inputs-form');
+
+    const loginInput = document.createElement('input');
+    loginInput.type = 'text';
+    loginInput.classList.add('input-login');
+    loginInput.placeholder = 'username';
+    const passwordInput = document.createElement('input');
+    passwordInput.type = 'password';
+    passwordInput.classList.add('input-password');
+    passwordInput.placeholder = 'password';
+    inputsForm.append(loginInput, passwordInput);
+
+    const loginButton = document.createElement('button');
+    loginButton.classList.add('login-btn');
+    loginButton.textContent = 'Login';
+    const p = document.createElement('p');
+    p.classList.add('error');
+
+    authForm.append(inputsForm, loginButton, p);
+    container.appendChild(authForm);
+
+    async function auth(e) {
+      const username = loginInput.value;
+      const password = passwordInput.value;
+      if (!!username && !!password) {
+        const auth = await request('/authenticate', 'POST', { username, password });
+        if (auth.message) {
+          p.style.display = 'block';
+          p.textContent = auth.message;
+          loginInput.value = '';
+          passwordInput.value = '';
+        }
+        else {
+          //-----------------------------------localStorage---------
+          localStorage.token = auth.token;
+          localStorage.id = auth.id;
+          token = localStorage.token;
+          userId = localStorage.id;
+          authForm.style.display = 'none';
+          createLogoutButton(container);
+          getTodos(userId);
+          newTodo.style.visibility = 'visible';
+        }
+      }
+    }
+
+    passwordInput.addEventListener('keypress', (e) => {
+      if (e.key === 'Enter') {
+        auth(e);
+      }
+    })
+
+    loginButton.addEventListener('click', e => auth(e));
+
+    assignEventHandlersForm(loginInput);
+    assignEventHandlersForm(passwordInput);
+
+    function assignEventHandlersForm(elem) {
+      elem.addEventListener('focus', e => {
+        p.textContent = '';
+        p.style.display = 'none';
+      })
+    }
+  }
+
+  function createLogoutButton(container) {
+    const logout = document.createElement('button');
+    logout.classList.add('login-btn');
+    logout.textContent = 'Logout';
+    container.prepend(logout);
+
+    logout.addEventListener('click', () => {
+      localStorage.clear();
+      todosList = [];
+      tasksList = [];
+      while (container.firstChild) {
+        container.removeChild(container.firstChild);
+      }
+      createAuthForm(container);
+      newTodo.style.visibility = 'hidden';
+    })
+  }
+
+  async function getTodos(id) {
+    todos = await request(`/todo/${id}`);
+    const todosList = todos.todos;
+    const tasksList = todos.tasks;
+
+    for (let i = 0; i < todosList.length; i++) {
+      const todo = todosList[i];
+      createTodo(todo, tasksList);
+    }
+  }
+
+  newTodo.addEventListener('click', async () => {
+    const newTodo = await request(`/todo/${userId}`, 'POST', { title: 'New TODO List' });
+    if (newTodo) {
+      todosList.push(newTodo);
+      createTodo(newTodo);
+    }
+  });
+
+  function createTodo(todo, tasks) {
+    const todoElem = document.createElement('div');
+    todoElem.classList.add('todo');
+    todoElem.dataset.id = todo.id;
+
+    const heading = document.createElement('div');
+    heading.classList.add('row', 'heading');
+
+    const row = document.createElement('div');
+    row.classList.add('row');
+    const todoIcon = document.createElement('i');
+    todoIcon.classList.add('icon', 'notepad', 'far', 'fa-list-alt');
+    const header = document.createElement('h2');
+    header.textContent = todo.title;
+    row.append(todoIcon, header);
+
+    const icons = document.createElement('span');
+    icons.classList.add('icons');
+    const pencilIcon = document.createElement('i');
+    pencilIcon.id = 'pencil';
+    pencilIcon.classList.add('icon', 'fas', 'fa-pencil-alt');
+    const trashIcon = document.createElement('i');
+    trashIcon.id = 'trash';
+    trashIcon.classList.add('icon', 'fas', 'fa-trash-alt');
+    icons.append(pencilIcon, trashIcon);
+
+    heading.append(row, icons);
+
+    const inputRow = document.createElement('div');
+    inputRow.classList.add('row', 'add-task-row');
+
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('icon-plus-wrapper');
+    const plusIcon = document.createElement('span');
+    plusIcon.classList.add('icon-plus');
+    wrapper.appendChild(plusIcon);
+
+    const inputField = document.createElement('div');
+    inputField.classList.add('input-field');
+    const input = document.createElement('input');
+    input.classList.add('add-task-input');
+    input.type = 'text';
+    input.placeholder = 'Start typing here to create a task';
+    const button = document.createElement('button');
+    button.classList.add('add-task-btn');
+    button.textContent = 'Add Task';
+    inputField.append(input, button);
+
+    inputRow.append(wrapper, inputField);
+
+    const ul = document.createElement('ul');
+    ul.classList.add('todos');
+
+    todoElem.append(heading, inputRow, ul);
+    container.appendChild(todoElem);
+
+    assignEventHandlers(todoElem);
+
+    if (tasks) {
+      const todoTasks = tasks.filter(task => task.todoId === todo.id);
+      todoTasks.map(task => createTask(task));
+    }
+  }
+
+  function assignEventHandlers(todo) {
+    const h2 = todo.querySelector('h2');
+    const tasks = Array.prototype.slice.call(todo.querySelectorAll('.task-text'));
+
+    todo.addEventListener('click', async (e) => {
+      const elem = e.target;
+      const todoElem = e.currentTarget;
+      const todoElemId = todoElem.dataset.id;
+      if (elem.classList.contains('add-task-btn')) {
+        const input = todo.querySelector('.add-task-input');
+        if (!!input.value) {
+          const newTask = await request(`/task/${todoElemId}`, 'POST', { text: input.value });
+          tasksList.push(newTask);
+          createTask(newTask);
+          input.value = '';
+        }
+      }
+      else if (elem.classList.contains('icon-plus-wrapper') || elem.classList.contains('icon-plus')) {
+        todo.querySelector('.add-task-input').focus();
+      }
+      else if (elem.id === 'pencil') {
+        h2.contentEditable = true;
+        h2.focus();
+      }
+      else if (elem.id === 'trash') {
+        await request(`/todo/${todoElemId}`, 'DELETE');
+        todoElem.remove();
+      }
+      else if (elem.classList.contains('fa-pencil-alt')) {
+        const task = elem.closest('li').querySelector('.task-text');
+        task.contentEditable = true;
+        task.focus();
+      }
+      else if (elem.classList.contains('fa-trash-alt') || elem.classList.contains('task-trash')) {
+        const task = elem.closest('li');
+        const taskId = task.dataset.id;
+        await request(`/task/${taskId}`, 'DELETE');
+        task.remove();
+      }
+      else if (elem.classList.contains('arrow-up-wrapper')) {
+        const currentTask = elem.closest('li');
+        const previousTask = elem.closest('li').previousElementSibling;
+        if (!!previousTask) {
+          const cloneCurrentTask = currentTask.cloneNode(true);
+          const clonePreviousTask = previousTask.cloneNode(true);
+          const parentElement = currentTask.parentElement;
+          parentElement.replaceChild(cloneCurrentTask, previousTask);
+          parentElement.replaceChild(clonePreviousTask, currentTask);
+        }
+      }
+      else if (elem.classList.contains('arrow-down-wrapper')) {
+        const currentTask = elem.closest('li');
+        const nextTask = elem.closest('li').nextElementSibling;
+        if (!!nextTask) {
+          const cloneCurrentTask = currentTask.cloneNode(true);
+          const cloneNextTask = nextTask.cloneNode(true);
+          const parentElement = currentTask.parentElement;
+          parentElement.replaceChild(cloneCurrentTask, nextTask);
+          parentElement.replaceChild(cloneNextTask, currentTask);
+        }
+      }
+      else if (elem.classList.contains('arrow-up')) {
+        const currentTask = elem.closest('li');
+        const previousTask = elem.closest('li').previousElementSibling;
+        if (!!previousTask) {
+          const cloneCurrentTask = currentTask.cloneNode(true);
+          const clonePreviousTask = previousTask.cloneNode(true);
+          const parentElement = currentTask.parentElement;
+          parentElement.replaceChild(cloneCurrentTask, previousTask);
+          parentElement.replaceChild(clonePreviousTask, currentTask);
+        }
+      }
+      else if (elem.classList.contains('arrow-down')) {
+        const currentTask = elem.closest('li');
+        const nextTask = elem.closest('li').nextElementSibling;
+        if (!!nextTask) {
+          const cloneCurrentTask = currentTask.cloneNode(true);
+          const cloneNextTask = nextTask.cloneNode(true);
+          const parentElement = currentTask.parentElement;
+          parentElement.replaceChild(cloneCurrentTask, nextTask);
+          parentElement.replaceChild(cloneNextTask, currentTask);
+        }
+      }
+
+    });
+
+    todo.addEventListener('keypress', async (e) => {
+      const elem = e.target;
+      const todoElem = e.currentTarget;
+      if (e.keyCode === 'Enter') {
+        if (elem.tagName === 'H2' || elem.classList.contains('task-text')) {
+          elem.blur();
+        }
+        else if (elem.classList.contains('add-task-input')) {
+          if (!!elem.value) {
+            const newTask = await request(`/task/${todoElem.dataset.id}`, 'POST', { text: elem.value });
+            if (newTask) {
+              tasksList.push(newTask);
+              createTask(newTask);
+              elem.value = '';
+              elem.blur();
+            }
+          }
+        }
+      }
+    });
+
+    todo.addEventListener('focusout', async (e) => {
+      const elem = e.target;
+      const text = elem.textContent;
+      if (elem.tagName === 'H2') {
+        const todoListId = e.currentTarget.dataset.id;
+        const todoList = todosList.find(t => t.id === todoListId);
+        await request(`/todo/${todoListId}`, 'PUT', { ...todoList, title: text });
+        elem.contentEditable = false;
+      }
+      else if (elem.classList.contains('task-text')) {
+        const taskId = elem.parentElement.parentElement.dataset.id;
+        const task = tasksList.find(t => t.id === taskId);
+        await request(`/task/${taskId}`, 'PUT', { ...task, text });
+        elem.contentEditable = false;
+      }
+    });
+  }
+
+  function createTask(newTask) {
+    if (!!newTask) {
+      const todo = document.querySelector(`div[data-id='${newTask.todoId}']`);
+      const ul = todo.querySelector('.todos');
+      const li = document.createElement('li');
+      li.classList.add('list-item');
+      li.dataset.id = newTask.id;
+
+      const label = document.createElement('label');
+      const checkboxSpan = document.createElement('span');
+      checkboxSpan.classList.add('checkbox');
+      const checkboxInput = document.createElement('input');
+      checkboxInput.type = 'checkbox';
+      checkboxInput.checked = newTask.marked;
+      checkboxSpan.appendChild(checkboxInput);
+
+      const textSpan = document.createElement('span');
+      textSpan.classList.add('task-text');
+      textSpan.tabIndex = '-1';
+      textSpan.append(newTask.text);
+      label.append(checkboxSpan, textSpan);
+
+      const iconsSpan = document.createElement('span');
+      iconsSpan.classList.add('icons');
+
+      const movingSpan = document.createElement('span');
+      movingSpan.classList.add('icon', 'task-moving');
+
+      const arrowUpSpan = document.createElement('span');
+      arrowUpSpan.classList.add('arrow-wrapper', 'arrow-up-wrapper');
+      const arrowUp = document.createElement('i');
+      arrowUp.classList.add('arrow-up');
+      arrowUpSpan.appendChild(arrowUp);
+
+      const separator = document.createElement('span');
+      separator.classList.add('separator');
+
+      const arrowDownSpan = document.createElement('span');
+      arrowDownSpan.classList.add('arrow-wrapper', 'arrow-down-wrapper');
+      const arrowDown = document.createElement('i');
+      arrowDown.classList.add('arrow-down');
+      arrowDownSpan.appendChild(arrowDown);
+
+      movingSpan.append(arrowUpSpan, separator, arrowDownSpan);
+
+      const editSpan = document.createElement('span');
+      editSpan.classList.add('icon', 'task-edit');
+      const editIcon = document.createElement('i');
+      editIcon.classList.add('fas', 'fa-pencil-alt');
+      editSpan.appendChild(editIcon);
+
+      const trashSpan = document.createElement('span');
+      trashSpan.classList.add('icon', 'task-trash');
+      const trashIcon = document.createElement('i');
+      trashIcon.classList.add('fas', 'fa-trash-alt');
+      trashSpan.appendChild(trashIcon);
+
+      iconsSpan.append(movingSpan, editSpan, trashSpan);
+
+      ul.appendChild(li).append(label, iconsSpan);
+
+      checkboxInput.addEventListener('change', async (e) => {
+        const elem = e.target;
+        const taskId = elem.closest('.list-item').dataset.id;
+        const task = tasksList.find(t => t.id === taskId);
+        await request(`/task/${taskId}`, 'PUT', { ...task, checked: elem.checked });
+      })
+    }
+  }
+
+  async function request(url, method = 'GET', data = null) {
+    const headers = {};
+    let body;
+    if (localStorage.token) {
+      headers['Authorization'] = `Bearer ${token}`;
+    }
+    if (data) {
+      headers['Content-Type'] = 'application/json';
+      body = JSON.stringify({ ...data });
+    }
+    try {
+      const response = await fetch(url, {
+        method,
+        headers,
+        body
+      });
+      return await response.json();
+    } catch (e) {
+      console.warn('Error:', e.message);
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', onPageLoaded);
---
+++ server.js
@@ -0,0 +1,182 @@
+const express = require('express');
+const path = require('path');
+const { v4 } = require('uuid');
+const app = express();
+const port = 4000;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const expressJwt = require('express-jwt');
+const jwt = require('jsonwebtoken');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+const config = {
+  secret: `;dtn',kznm`,
+  algorithms: ['HS256']
+}
+
+function jwtWare() {
+  const { secret } = config;
+  return expressJwt(config).unless({
+    path: [
+      '/authenticate',
+      '/',
+      '/style.css',
+      '/script.js'
+    ]
+  });
+}
+
+function errorHandler(err, req, res, next) {
+  if (typeof (err) === 'string') {
+    return res.status(400).json({ message: err });
+  }
+  if (err.name === 'UnauthorizedError') {
+    return res.status(401).json({ message: 'Invalid Token' });
+  }
+  return res.status(500).json({ message: err.message });
+}
+
+async function authenticate({ username, password }) {
+  const user = await User.findOne({ where: { username, password } });
+  if (user) {
+    const token = jwt.sign({ sub: user.id }, config.secret);
+    const { password, ...userWithoutPassword } = user;
+    return {
+      ...userWithoutPassword,
+      token
+    };
+  }
+  else {
+    console.log("not found");
+  }
+}
+
+// api routes
+app.post('/authenticate', function (req, res, next) {
+  authenticate(req.body)
+    .then(user => user ? res.json(user) : res.status(400).json({ message: 'Username or password is incorrect' }))
+    .catch(err => next(err));
+});
+
+app.use(jwtWare());
+
+app.use(errorHandler);
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'client')));
+
+app.get('/', (req, res, next) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
+  next();
+})
+
+//GET
+app.get('/todo/:id', async (req, res) => {
+  const todos = await getTodos(req.params.id);
+  if (todos) {
+    const tasks = [];
+    for (let i = 0; i < todos.length; i++) {
+      tasks.push(...await getTasks(todos[i].id));
+    }
+    if (tasks) {
+      res.send({ todos, tasks });
+    }
+    else {
+      res.send(todos);
+    }
+  }
+  else {
+    res.status(400).send({ message: "Todos not found" });
+  }
+});
+
+//POST
+app.post('/todo/:userId', async (req, res) => {
+  const todo = await Todo.create({ userId: req.params.userId, ...req.body });
+  res.status(201).json(todo);
+});
+app.post('/task/:todoId', async (req, res) => {
+  const task = await Task.create({ todoId: req.params.todoId, ...req.body });
+  res.status(201).json(task);
+});
+
+//DELETE
+app.delete('/todo/:id', async (req, res) => {
+  const task = await Task.destroy({ where: { todoId: req.params.id } });
+  const todo = await Todo.destroy({ where: { id: req.params.id } });
+  res.status(200).json({ message: 'success' });
+});
+app.delete('/task/:id', async (req, res) => {
+  const task = await Task.destroy({ where: { id: req.params.id } });
+  res.status(200).json({ message: 'success' });
+});
+
+//PUT
+app.put('/todo/:id', async (req, res) => {
+  const todo = await Todo.update({ title: req.body.title }, { where: { id: req.params.id } });
+  res.status(200).json({ message: "success" });
+});
+app.put('/task/:id', async (req, res) => {
+  const task = await Task.update({ text: req.body.text, marked: Number(req.body.checked) }, { where: { id: req.params.id } });
+  res.status(200).json({ message: "success" });
+});
+
+async function getTodos(id) {
+  const todos = await Todo.findAll({ where: { userId: id } });
+  return todos;
+}
+
+async function getTasks(id) {
+  const tasks = await Task.findAll({ where: { todoId: id } });
+  return tasks;
+}
+
+// ------------------	Sequelize
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('mysql://user:12345@localhost/todo_task', { query: { raw: true } });
+
+try {
+  sequelize.authenticate()
+    .then(() => console.log('Connection has been established successfully.'));
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
+class User extends Sequelize.Model { }
+
+User.init({
+  username: Sequelize.STRING,
+  password: Sequelize.STRING
+}, { sequelize, modelName: 'user' });
+
+class Todo extends Sequelize.Model {
+  get tasks() {
+    const tasks = getTasks();
+    return tasks;
+  }
+}
+
+Todo.init({
+  title: Sequelize.STRING
+}, { sequelize, modelName: 'todo' });
+
+class Task extends Sequelize.Model { }
+
+Task.init({
+  text: Sequelize.TEXT,
+  marked: Sequelize.TINYINT
+}, { sequelize, modelName: 'task' });
+
+User.hasMany(Todo);
+Todo.belongsTo(User);
+Todo.hasMany(Task);
+Task.belongsTo(Todo);
+
+; (async () => {
+  await sequelize.sync()
+})();
+
+app.listen(port, () => { console.log(`Server has been started on port ${port}...`) });
